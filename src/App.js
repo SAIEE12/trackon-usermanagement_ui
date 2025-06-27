4df9e96abfd311244ba0f55c7b46e7dc56f3d25a
Manish-A <saimanishsai19189@gmail.com>
2025-06-27 12:10:37 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserManagementPage from './pages/UserManagementPage';
+import ClientManagementPage from './pages/ClientManagementPage'; 
+import Layout from './components/Layout';
 
-function App() {
+const withLayout = (Component) => <Layout><Component /></Layout>;
+
+const App = () => {
   return (
-    <div className="bg-gray-50 min-h-screen">
-      <UserManagementPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={withLayout(UserManagementPage)} />
+        <Route path="/clients" element={withLayout(ClientManagementPage)} />  
+        <Route path="/candidates" element={withLayout(() => <div>Candidate Management</div>)} />
+        <Route path="/requirements" element={withLayout(() => <div>Requirements Tracking</div>)} />
+        <Route path="/internal-interviews" element={withLayout(() => <div>Internal Interviews</div>)} />
+        <Route path="/external-mappings" element={withLayout(() => <div>External Mappings</div>)} />
+        <Route path="/analytics" element={withLayout(() => <div>Reports & Analytics</div>)} />
+        <Route path="/settings" element={withLayout(() => <div>Settings</div>)} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
